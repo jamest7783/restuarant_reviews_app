1ecfe117854ccc6f4b55c53c63a00cf3a040bd83
James Jewitt <jamestjewitt@gmail.com>
2022-08-01 11:24:05 -0400

    Get a single restuarant

--- server.js
+++ server.js
@@ -7,26 +7,34 @@ const {Restuarant}=require('./models')
  
 
 // MIDDLE WARE
-
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(express.static(`${__dirname}/client/build`))
+
+
+
+
 
 // ROUTES
 app.get('/restuarants', async (req,res)=>{
     const restuarants = await Restuarant.find({})
     res.send(restuarants)
 })
-
 app.post('/restuarants',async (req,res)=>{
-    //res.send(req.body)
     let createdRestuarant=await Restuarant.create(req.body)
     res.send(createdRestuarant)
 })
-
+app.get('/restuarants/:id',async (req,res)=>{
+    let foundRestuarant=await Restuarant.findById(req.params.id)
+    res.send(foundRestuarant)
+})
 app.get('/*', (req, res) => {
     res.sendFile(`${__dirname}/client/build/index.html`)
 })
+
+
+
+
 
 app.listen(PORT,()=>{
     console.log(`I am currently running at PORT:${PORT}`)
